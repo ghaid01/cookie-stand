59f4cfb0a3685a86a59b77a4f07566b1b81ecdb0
ghaid01 <ghaid.zahran2001@gmail.com>
2019-10-13 22:02:24 +0300

    incomplete

---
+++ app.js
@@ -0,0 +1,67 @@
+var Seattle = {
+    randomInRange :function() {
+        var range = this.maxcust - this.mincust;
+var rand = (math.random() * range) + this.mincust;
+return math.ceil(rand);
+    },
+    
+mincust :23,
+maxcust :65,
+avgcookie :6.3,
+
+};
+var Tokyo = {
+    randomInRange :function() {
+        var range = this.maxcust - this.mincust;
+var rand = (math.random() * range) + mincust;
+return math.ceil(rand);
+    },
+mincust :3,
+maxcust :24,
+avgcookie :1.2,
+
+}
+var Dubai = {
+    randomInRange :function() {
+        var range = this.maxcust - this.mincust;
+var rand = (math.random() * range) + mincust;
+return math.ceil(rand);
+    },
+    mincust :11,
+    maxcust :38,
+    avgcookie :3.7,
+
+}
+var Paris = {
+    randomInRange :function() {
+        var range = this.maxcust - this.mincust;
+var rand = (math.random() * range) + mincust;
+return math.ceil(rand);
+    },
+    mincust :20,
+    maxcust :38,
+    avgcookie :2.3,
+}
+var Lima = { 
+    randomInRange :function() {
+        var range = this.maxcust - this.mincust;
+var rand = (math.random() * range) + mincust;
+return math.ceil(rand);
+    },
+mincust :2,
+maxcust :16,
+avgcookie :4.6,
+
+}
+var shops = [Seattle,Tokyo,Dubai,Paris,Lima];
+var times = ['6am','7am','8am','9am','10am','11am','12am'];
+
+for (var i = 0; i < shops.length; i++) {
+    
+}
+    
+for (var g=0; g < times.legnth; g++) {
+
+    
+
+}
